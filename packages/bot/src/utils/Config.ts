6cab2cd080fd64682a8d347c744abcc4f8cc52f3
Bare7a <bare7a@gmail.com>
2023-04-30 22:30:35 +0300

    Pezi Bot

--- packages/bot/src/utils/Config.ts
+++ packages/bot/src/utils/Config.ts
@@ -2,7 +2,14 @@ import path from 'path';
 import { ConfigType } from '../types';
 import { config } from 'dotenv';
 
-const env = config().parsed ?? config({ path: path.resolve('../../.env') }).parsed;
+const dirs = { dir1: process.cwd(), dir2: path.normalize('../../') };
+const envs = {
+  env1: config({ path: path.join(dirs.dir1, '.env') }).parsed,
+  env2: config({ path: path.join(dirs.dir2, '.env') }).parsed,
+};
+
+const env = envs.env1 ?? envs.env2;
+const dir = envs.env1 ? dirs.dir1 : dirs.dir2;
 if (!env) throw Error(`Couldn't find an .env file in the current directory`);
 
 export const CONFIG: ConfigType = {
@@ -14,8 +21,8 @@ export const CONFIG: ConfigType = {
   refreshToken: env.bot_refresh_token ?? 'NOT_DEFINED',
   currencyName: env.bot_currency_name ?? 'NOT_DEFINED',
   defaultPoints: Number(env.bot_default_points ?? -1337),
-  dbPath: env.bot_db_path ?? path.join(process.cwd(), 'db.sqlite'),
-  logPath: env.bot_db_path ?? path.join(process.cwd(), 'log.sqlite'),
+  dbPath: env.bot_db_path ?? path.join(dir, 'db.sqlite'),
+  logPath: env.bot_db_path ?? path.join(dir, 'log.sqlite'),
 };
 
 if (['NOT_DEFINED', -1337].some((invalidValue) => Object.values(CONFIG).includes(invalidValue)))
