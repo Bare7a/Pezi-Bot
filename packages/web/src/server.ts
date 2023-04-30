fdf7afa261d3ab158487a988694c91a14302fd65
Bare7a <bare7a@gmail.com>
2023-04-30 22:55:32 +0300

    Pezi Bot

--- packages/web/src/server.ts
+++ packages/web/src/server.ts
@@ -5,7 +5,6 @@ import { createServer } from 'http';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
-console.log(process.env);
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
