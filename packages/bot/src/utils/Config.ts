a3397b28838b389d4ed14abad31adee296b57baf
Bare7a <bare7a@gmail.com>
2023-04-30 22:13:27 +0300

    Pezi Bot

--- packages/bot/src/utils/Config.ts
+++ packages/bot/src/utils/Config.ts
@@ -1,19 +1,21 @@
 import path from 'path';
 import { ConfigType } from '../types';
 import { config } from 'dotenv';
-config();
+
+const env = config().parsed ?? config({ path: path.resolve('../../.env') }).parsed;
+if (!env) throw Error(`Couldn't find an .env file in the current directory`);
 
 export const CONFIG: ConfigType = {
-  streamer: process.env.bot_streamer ?? 'NOT_DEFINED',
-  username: process.env.bot_username ?? 'NOT_DEFINED',
-  password: process.env.bot_password ?? 'NOT_DEFINED',
-  clientId: process.env.bot_client_id ?? 'NOT_DEFINED',
-  accessToken: process.env.bot_access_token ?? 'NOT_DEFINED',
-  refreshToken: process.env.bot_refresh_token ?? 'NOT_DEFINED',
-  currencyName: process.env.bot_currency_name ?? 'NOT_DEFINED',
-  defaultPoints: Number(process.env.bot_default_points ?? -1337),
-  dbPath: process.env.bot_db_path ?? path.join(process.cwd(), 'db.sqlite'),
-  logPath: process.env.bot_db_path ?? path.join(process.cwd(), 'log.sqlite'),
+  streamer: env.bot_streamer ?? 'NOT_DEFINED',
+  username: env.bot_username ?? 'NOT_DEFINED',
+  password: env.bot_password ?? 'NOT_DEFINED',
+  clientId: env.bot_client_id ?? 'NOT_DEFINED',
+  accessToken: env.bot_access_token ?? 'NOT_DEFINED',
+  refreshToken: env.bot_refresh_token ?? 'NOT_DEFINED',
+  currencyName: env.bot_currency_name ?? 'NOT_DEFINED',
+  defaultPoints: Number(env.bot_default_points ?? -1337),
+  dbPath: env.bot_db_path ?? path.join(process.cwd(), 'db.sqlite'),
+  logPath: env.bot_db_path ?? path.join(process.cwd(), 'log.sqlite'),
 };
 
 if (['NOT_DEFINED', -1337].some((invalidValue) => Object.values(CONFIG).includes(invalidValue)))
